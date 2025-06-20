558546a04c632aa36f3097258119a3e76d1a5277
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-06-20 15:13:21 +0200

    Reindent with spaces

--- plugin/Filters/utils/DGtalVTKImage.h
+++ plugin/Filters/utils/DGtalVTKImage.h
@@ -14,156 +14,156 @@
 struct DGtalVTKImage : public DGtalVTKAbstractContainer
 {
 public:
-	static DGtalVTKImage Empty()
-	{
-		DGtalVTKImage img;
-		return img;
-	}
+    static DGtalVTKImage Empty()
+    {
+        DGtalVTKImage img;
+        return img;
+    }
 
-	static DGtalVTKImage CreateFromBounds(
-		const double imageBounds[6], 
-		const double cellBounds[6], 
-		const unsigned int nbCells
-	) 
-	{
-		DGtalVTKImage image;
-		image.SetBounds(imageBounds, cellBounds, nbCells);
+    static DGtalVTKImage CreateFromBounds(
+        const double imageBounds[6], 
+        const double cellBounds[6], 
+        const unsigned int nbCells
+    ) 
+    {
+        DGtalVTKImage image;
+        image.SetBounds(imageBounds, cellBounds, nbCells);
 
-		image.domain = DGtal::CountedPtr<SH3::Domain>( new SH3::Domain(
-			SH3::Point(std::floor(imageBounds[0] / image.cellSize[0]), 
-				       std::floor(imageBounds[2] / image.cellSize[1]), 
-				       std::floor(imageBounds[4] / image.cellSize[2])), 
-			SH3::Point(std::ceil (imageBounds[1] / image.cellSize[0]), 
-				       std::ceil (imageBounds[3] / image.cellSize[1]), 
-				       std::ceil (imageBounds[5] / image.cellSize[2]))
-		));
-		image.image = DGtal::CountedPtr<SH3::BinaryImage>(new SH3::BinaryImage(*image.domain));
+        image.domain = DGtal::CountedPtr<SH3::Domain>( new SH3::Domain(
+            SH3::Point(std::floor(imageBounds[0] / image.cellSize[0]), 
+                       std::floor(imageBounds[2] / image.cellSize[1]), 
+                       std::floor(imageBounds[4] / image.cellSize[2])), 
+            SH3::Point(std::ceil (imageBounds[1] / image.cellSize[0]), 
+                       std::ceil (imageBounds[3] / image.cellSize[1]), 
+                       std::ceil (imageBounds[5] / image.cellSize[2]))
+        ));
+        image.image = DGtal::CountedPtr<SH3::BinaryImage>(new SH3::BinaryImage(*image.domain));
 
-		return image;
-	}
+        return image;
+    }
 
-	static DGtalVTKImage CreateFromImage(
-		DGtal::CountedPtr<SH3::BinaryImage> im, 
-		const double cellSize = 1.0
-	)
-	{
-		DGtalVTKImage image;
+    static DGtalVTKImage CreateFromImage(
+        DGtal::CountedPtr<SH3::BinaryImage> im, 
+        const double cellSize = 1.0
+    )
+    {
+        DGtalVTKImage image;
 
-		image.domain = DGtal::CountedPtr<SH3::Domain>(new SH3::Domain(im->domain()));
-		image.image = im;
+        image.domain = DGtal::CountedPtr<SH3::Domain>(new SH3::Domain(im->domain()));
+        image.image = im;
 
-		auto l = image.domain->lowerBound();
-		auto u = image.domain->upperBound();
+        auto l = image.domain->lowerBound();
+        auto u = image.domain->upperBound();
 
-		const double imgsBounds[6] = {
-			l[0] * cellSize, u[0] * cellSize,
-			l[1] * cellSize, u[1] * cellSize,
-			l[2] * cellSize, u[2] * cellSize
-		};
-		const double cellBounds[6] = {0, cellSize, 0, cellSize, 0, cellSize};
-		unsigned int nbCells = std::count(image.image->begin(), image.image->end(), true);
-		image.SetBounds(imgsBounds, cellBounds, nbCells);
+        const double imgsBounds[6] = {
+            l[0] * cellSize, u[0] * cellSize,
+            l[1] * cellSize, u[1] * cellSize,
+            l[2] * cellSize, u[2] * cellSize
+        };
+        const double cellBounds[6] = {0, cellSize, 0, cellSize, 0, cellSize};
+        unsigned int nbCells = std::count(image.image->begin(), image.image->end(), true);
+        image.SetBounds(imgsBounds, cellBounds, nbCells);
 
-		return image;
-	}
+        return image;
+    }
 
-	operator bool() { return image.isValid() && domain.isValid(); }
+    operator bool() { return image.isValid() && domain.isValid(); }
 
-	void SetVoxel(const SH3::Point& point)
-	{
-		image->setValue(point, true);
-	}
+    void SetVoxel(const SH3::Point& point)
+    {
+        image->setValue(point, true);
+    }
 
-	void SetVoxel(const double coords[3])
-	{
-		const SH3::Point p(std::round(coords[0] / cellSize[0]),
-			               std::round(coords[1] / cellSize[1]),
-				           std::round(coords[2] / cellSize[2]));
-		SetVoxel(p);
-	}
+    void SetVoxel(const double coords[3])
+    {
+        const SH3::Point p(std::round(coords[0] / cellSize[0]),
+                           std::round(coords[1] / cellSize[1]),
+                           std::round(coords[2] / cellSize[2]));
+        SetVoxel(p);
+    }
 
 
-	DGtal::CountedPtr<SH3::BinaryImage> GetImage() const
-	{
-		return image;
-	}
-	
+    DGtal::CountedPtr<SH3::BinaryImage> GetImage() const
+    {
+        return image;
+    }
+    
 
-	// Iterating facilities
+    // Iterating facilities
 public:
-	struct Iterator : public DgtalToVtkIterator::IteratorImpl
-	{
-		using BaseIterator = SH3::Domain::ConstIterator;
+    struct Iterator : public DgtalToVtkIterator::IteratorImpl
+    {
+        using BaseIterator = SH3::Domain::ConstIterator;
 
-		Iterator(
-			const DGtalVTKImage* im,
-			const BaseIterator& it
-		) : it(it), image(im)
-		{ }
+        Iterator(
+            const DGtalVTKImage* im,
+            const BaseIterator& it
+        ) : it(it), image(im)
+        { }
 
-		void Increment() override
-		{
-			++it;
-		}
-		
-		void Advance() override
-		{
-			while(it != image->domain->end())
-			{
-				const unsigned int idx = image->image->linearized(*it);
-				
-				if (image->image->at(idx)) break;
-				else ++it;
-			} 
-		}
+        void Increment() override
+        {
+            ++it;
+        }
+        
+        void Advance() override
+        {
+            while(it != image->domain->end())
+            {
+                const unsigned int idx = image->image->linearized(*it);
+                
+                if (image->image->at(idx)) break;
+                else ++it;
+            } 
+        }
 
-		bool Compare(const DgtalToVtkIterator::IteratorImpl* other) const override
-		{
-			const Iterator* otherIt = dynamic_cast<const Iterator*>(other);
-			
-			if (!otherIt) return false;
-			return (it == otherIt->it);
-		}
+        bool Compare(const DgtalToVtkIterator::IteratorImpl* other) const override
+        {
+            const Iterator* otherIt = dynamic_cast<const Iterator*>(other);
+            
+            if (!otherIt) return false;
+            return (it == otherIt->it);
+        }
 
-		void FillCellCoordinates(double* coords) const override
-		{
-			coords[0] = (*it)[0] * image->cellSize[0];
-			coords[1] = (*it)[1] * image->cellSize[1]; 
-			coords[2] = (*it)[2] * image->cellSize[2];
-		}
-		
-		const DGtalVTKImage* image;
-		BaseIterator it;
-	};
+        void FillCellCoordinates(double* coords) const override
+        {
+            coords[0] = (*it)[0] * image->cellSize[0];
+            coords[1] = (*it)[1] * image->cellSize[1]; 
+            coords[2] = (*it)[2] * image->cellSize[2];
+        }
+        
+        const DGtalVTKImage* image;
+        BaseIterator it;
+    };
 
 
-	DGtalVTKAbstractContainer::DgtalToVtkIterator begin() const override
-	{
-		return DgtalToVtkIterator(new Iterator(this, domain->begin()));
-	}
+    DGtalVTKAbstractContainer::DgtalToVtkIterator begin() const override
+    {
+        return DgtalToVtkIterator(new Iterator(this, domain->begin()));
+    }
 
-	DGtalVTKAbstractContainer::DgtalToVtkIterator end() const 
-	{
-		return DgtalToVtkIterator(new Iterator(this, domain->end()));
-	}
+    DGtalVTKAbstractContainer::DgtalToVtkIterator end() const 
+    {
+        return DgtalToVtkIterator(new Iterator(this, domain->end()));
+    }
 private:
-	DGtal::CountedPtr<SH3::Domain> domain;
-	DGtal::CountedPtr<SH3::BinaryImage> image;
+    DGtal::CountedPtr<SH3::Domain> domain;
+    DGtal::CountedPtr<SH3::BinaryImage> image;
 };
 
 /*
 
 
 
-		
+        
 
-		const double* operator*()
-		{
-			currentCell[0] = (*it)[0] * image->cellSize[0];
-			currentCell[1] = (*it)[1] * image->cellSize[1]; 
-			currentCell[2] = (*it)[2] * image->cellSize[2];
+        const double* operator*()
+        {
+            currentCell[0] = (*it)[0] * image->cellSize[0];
+            currentCell[1] = (*it)[1] * image->cellSize[1]; 
+            currentCell[2] = (*it)[2] * image->cellSize[2];
 
-			return currentCell;
-		}
+            return currentCell;
+        }
 
 */
--- plugin/Filters/utils/DGtalVTKSurface.h
+++ plugin/Filters/utils/DGtalVTKSurface.h
@@ -12,77 +12,77 @@
 class DGtalVTKSurface : public DGtalVTKAbstractContainer
 {
 public:
-	DGtalVTKSurface(const DGtalVTKImage& im) :
-		image(im.GetImage())
-	{ 
-		auto params = SH3::defaultParameters() | SHG3::defaultParameters();
-		auto K = SH3::getKSpace(image, params);
+    DGtalVTKSurface(const DGtalVTKImage& im) :
+        image(im.GetImage())
+    { 
+        auto params = SH3::defaultParameters() | SHG3::defaultParameters();
+        auto K = SH3::getKSpace(image, params);
 
-		surface = SH3::makeLightDigitalSurface( image, K, params );
-		range   = SH3::getSurfelRange(surface, params);
-	
-		SetBounds(im);
-		nbCells = range.size();
-	}
+        surface = SH3::makeLightDigitalSurface( image, K, params );
+        range   = SH3::getSurfelRange(surface, params);
+    
+        SetBounds(im);
+        nbCells = range.size();
+    }
 
-	DGtal::CountedPtr<SH3::BinaryImage> GetImage() const
-	{
-		return image;
-	}
+    DGtal::CountedPtr<SH3::BinaryImage> GetImage() const
+    {
+        return image;
+    }
 
-	const SH3::SurfelRange& GetSurfelRange() const
-	{
-		return range;
-	}
+    const SH3::SurfelRange& GetSurfelRange() const
+    {
+        return range;
+    }
 
 public:
-	struct Iterator : public DgtalToVtkIterator::IteratorImpl
-	{
-		using BaseIterator = SH3::SurfelRange::const_iterator;
+    struct Iterator : public DgtalToVtkIterator::IteratorImpl
+    {
+        using BaseIterator = SH3::SurfelRange::const_iterator;
 
-		Iterator(
-			const DGtalVTKSurface* surf, 
-			const BaseIterator& it
-		) : it(it), surf(surf)
-		{ }
+        Iterator(
+            const DGtalVTKSurface* surf, 
+            const BaseIterator& it
+        ) : it(it), surf(surf)
+        { }
 
-		void Increment() override { ++it; }
+        void Increment() override { ++it; }
 
-		void Advance() override { }
+        void Advance() override { }
 
-		bool Compare(const DgtalToVtkIterator::IteratorImpl* other) const override
-		{
-			const Iterator* otherIt = dynamic_cast<const Iterator*>(other);
-			
-			if (!otherIt) return false;
-			return (it == otherIt->it);
-		}
+        bool Compare(const DgtalToVtkIterator::IteratorImpl* other) const override
+        {
+            const Iterator* otherIt = dynamic_cast<const Iterator*>(other);
+            
+            if (!otherIt) return false;
+            return (it == otherIt->it);
+        }
 
-		void FillCellCoordinates(double* coords) const override
-		{
-			// Don't know yet why I should multiply by 0.5 ? 
-			coords[0] = 0.5 * it->preCell().coordinates[0] * surf->cellSize[0];
-			coords[1] = 0.5 * it->preCell().coordinates[1] * surf->cellSize[1]; 
-			coords[2] = 0.5 * it->preCell().coordinates[2] * surf->cellSize[2];
-		}
-		
-		const DGtalVTKSurface* surf;
-		BaseIterator it;
-	};
+        void FillCellCoordinates(double* coords) const override
+        {
+            // Don't know yet why I should multiply by 0.5 ? 
+            coords[0] = 0.5 * it->preCell().coordinates[0] * surf->cellSize[0];
+            coords[1] = 0.5 * it->preCell().coordinates[1] * surf->cellSize[1]; 
+            coords[2] = 0.5 * it->preCell().coordinates[2] * surf->cellSize[2];
+        }
+        
+        const DGtalVTKSurface* surf;
+        BaseIterator it;
+    };
 
 
-	DGtalVTKAbstractContainer::DgtalToVtkIterator begin() const override
-	{
-		return DgtalToVtkIterator(new Iterator(this, range.begin()));
-	}
+    DGtalVTKAbstractContainer::DgtalToVtkIterator begin() const override
+    {
+        return DgtalToVtkIterator(new Iterator(this, range.begin()));
+    }
 
-	DGtalVTKAbstractContainer::DgtalToVtkIterator end() const 
-	{
-		return DgtalToVtkIterator(new Iterator(this, range.end()));
-	}
+    DGtalVTKAbstractContainer::DgtalToVtkIterator end() const 
+    {
+        return DgtalToVtkIterator(new Iterator(this, range.end()));
+    }
 
 private:
-	DGtal::CountedPtr<SH3::BinaryImage> image; // Hold pointer to avoid early destruction
-	DGtal::CountedPtr<SH3::LightDigitalSurface> surface; 
-	SH3::SurfelRange range;
+    DGtal::CountedPtr<SH3::BinaryImage> image; // Hold pointer to avoid early destruction
+    DGtal::CountedPtr<SH3::LightDigitalSurface> surface; 
+    SH3::SurfelRange range;
 };
